d1a11e9886ce9f69dbc16ab5fbe7aa4bc93dc0a2
Larry Holst <45205643+Diicorp95@users.noreply.github.com>
2021-02-13 16:52:21 +0300

    Update DOSLDR.asm

--- sources/boot/DOSLDR.asm
+++ sources/boot/DOSLDR.asm
@@ -1,72 +1,70 @@
-; Операционная система VH-DOS
-; Copyright(C) Саша Волохов, 2020.
+; РћРїРµСЂР°С†РёРѕРЅРЅР°СЏ СЃРёСЃС‚РµРјР° VH-DOS
+; Copyright(C) РЎР°С€Р° Р’РѕР»РѕС…РѕРІ, 2020.
 
-; Данный файл служит для запуска ОС, и загружает основные программы в память.
+; Р”Р°РЅРЅС‹Р№ С„Р°Р№Р» СЃР»СѓР¶РёС‚ РґР»СЏ Р·Р°РїСѓСЃРєР° РћРЎ, Рё Р·Р°РіСЂСѓР¶Р°РµС‚ РѕСЃРЅРѕРІРЅС‹Рµ РїСЂРѕРіСЂР°РјРјС‹ РІ РїР°РјСЏС‚СЊ.
 
-org 500h
+	org 500h
 
 message:
 	mov ax, 0002h
-        int 10h
+	int 10h
         
 	mov dx,0h
-        mov bp, start_dos
-        mov cx, 18
+        mov bp,start_dos
+        mov cx,18
         mov bl,07h					
         xor bh,bh
         mov ax,1301h
         int 10h
         mov si,0
 		
-		mov ah,02h
-		mov bh,0
-		mov dh, 2
-		mov dl, 0
-		int 10h
+	mov ah,02h
+	mov bh,0
+	mov dh, 2
+	mov dl, 0
+	int 10h
 		
-		mov ax,0000h
-		mov es,ax
-		mov bx,800h
-		mov ch,0
-		mov cl,04h
-		mov dh,0
-		mov	dl,80h
-		mov al,01h
-		mov ah,02h
-		int 13h ; чтение boot файла
+	mov ax,0000h
+	mov es,ax
+	mov bx,800h
+	mov ch,0
+	mov cl,04h
+	mov dh,0
+	mov dl,80h
+	mov al,01h
+	mov ah,02h
+	int 13h ; С‡С‚РµРЅРёРµ boot С„Р°Р№Р»Р°
 		
-		mov ax,0000h
-		mov es,ax
-		mov bx,600h
-		mov ch,0
-		mov cl,03h
-		mov dh,0
-		mov	dl,80h
-		mov al,01h
-		mov ah,02h
-		int 13h
-		
-		mov ax, 0002h
+	mov ax,0000h
+	mov es,ax
+	mov bx,600h
+	mov ch,0
+	mov cl,03h
+	mov dh,0
+	mov dl,80h
+	mov al,01h
+	mov ah,02h
+	int 13h
+	
+	mov ax, 0002h
         int 10h
 		
-		mov ah,02h
-		mov bh,0
-		mov dh,0
-		mov dl,0
-		int 10h
+	mov ah,02h
+	mov bh,0
+	mov dh,0
+	mov dl,0
+	int 10h
 		
-		jmp 0000:0600h
-		
-		
-jmp $
+	jmp 0000:0600h	
+	jmp $ ; Р’РѕР·РјРѕР¶РЅРѕ, РЅРёРєРѕРіРґР° РґРѕСЃСЋРґР° РїСЂРѕС†РµСЃСЃ РЅРµ РґРѕР№РґС‘С‚
 
 start_dos db 'Starting VH-DOS...',0
-error_start db 'Cannot find COMMAND.SYS'
+error_start db 'Cannot find COMMAND.SYS',0
 
 print_mes:
-mov bl,07h					
-xor bh,bh
-mov ax,1301h
-int 10h
-mov si,0
-ret
+	mov bl,07h					
+	xor bh,bh
+	mov ax,1301h
+	int 10h
+	mov si,0
+	ret
