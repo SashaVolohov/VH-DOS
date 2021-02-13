d81a938a4b36928745ad2f13245433eaf9b20fd6
Larry Holst <45205643+Diicorp95@users.noreply.github.com>
2021-02-13 16:56:09 +0300

    Update boot.asm

--- sources/boot/boot.asm
+++ sources/boot/boot.asm
@@ -1,5 +1,5 @@
 ; Операционная система VH-DOS
-; Copyright(C) Саша Волохов, 2020.
+; © Саша Волохов, 2020-2021.
 
 ; Данный файл служит для запуска ОС.
 ; Этот файл находится в MBR дискеты/жёсткого диска. Загружается по адресу 0x7c00
@@ -7,79 +7,73 @@
 org 7c00h
 
 start:
+	cli
+	xor ax,ax
+	mov ds,ax
+	mov es,ax
+	mov ss,ax
+	mov sp,07C00h
+	sti
+	
+	mov ax, 0002h
+	int 10h
+	mov ah,2
+	mov bh,0
+	; xor dx,dx ; Не требуется
+	int 10h
 
-cli
-xor ax,ax
-mov ds,ax
-mov es,ax
-mov ss,ax
-mov sp,07C00h
-sti
-   
-mov ax, 0002h
-int 10h
+	mov bp,fail_ldr
+	mov cx,18
 
-mov ah,02h
-mov bh,0
-mov dh,0
-mov dl,0
-int 10h
+	call print_mes
 
-mov bp,fail_ldr
-mov cx, 18
+	mov ah,2
+	mov bh,0
+	mov dh,1
+	mov dl,0
+	int 10h
 
-call print_mes
+	mov bp,fail_ldr_two
+	mov cx,29
+	call print_mes
 
-mov ah,02h
-mov bh,0
-mov dh,1
-mov dl,0
-int 10h
-
-mov bp,fail_ldr_two
-mov cx, 29
-
-call print_mes
-
-mov ax,0000h
-mov es,ax
-mov bx,500h
-mov ch,0
-mov cl,02h
-mov dh,0
-mov	dl,80h
-mov al,01h
-mov ah,02h
-int 13h
-jmp 0000:0500h
-
-call ClearMes
-
-mov ah,02h
-mov bh,0
-mov dh,2
-mov dl,0
-int 10h
-
-jmp $
-
+	mov ax,0
+	mov es,ax
+	mov bx,00500h
+	mov ch,0
+	mov cl,2
+	mov dh,0
+	mov dl,80d
+	mov al,1
+	mov ah,2
+	int 13h
+	jmp 0000:0500h
+	call ClearMes
+	
+	mov ah,2
+	mov bh,0
+	mov dh,2
+	mov dl,0
+	int 10h
+	
+	jmp $
+	
 print_mes:
-mov bl,07h					
-xor bh,bh
-mov ax,1301h
-int 10h
-mov si,0
-ret
+	mov bl,07h					
+	xor bh,bh
+	mov ax,1301h
+	int 10h
+	mov si,0
+	ret
 		
 ClearMes:
-	mov bp,null
+	mov bp,0
 	mov cx,0
 	call print_mes
-ret
-		
+	ret
+	
+;----
 fail_ldr db 'DOSLDR is missing.',0
-fail_ldr_two db 'Press Ctrl+Alt+Del to restart',0
-null db '',0
-
+fail_ldr_two db 'Press <Ctrl>-<Alt>-<Del> to restart',0
 times(512-2-($-07C00h)) db 0
 db 055h,0AAh
