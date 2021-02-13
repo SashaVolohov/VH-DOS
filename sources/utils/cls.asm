0b485d87555cf37a3baf1757ea110691ea221fc4
Larry Holst <45205643+Diicorp95@users.noreply.github.com>
2021-02-13 16:31:36 +0300

    Update cls.asm

--- sources/utils/cls.asm
+++ sources/utils/cls.asm
@@ -1,22 +1,21 @@
 org 800h
 
 message:
+  mov ax,2
+  int 10h
+  
+  mov ah,2
+  mov bh,0
+  mov dh,0
+  mov dl,0
+  int 10h
 
-mov ax, 0002h
-int 10h
-
-mov ah,02h
-mov bh,0
-mov dh,0
-mov dl,0
-int 10h
-
-jmp 0000:0600h
+  jmp 0000:0600h
 
 print_mes:
-mov bl,07h					
-xor bh,bh
-mov ax,1301h
-int 10h
-mov si,0
-ret
+  mov bl,07h					
+  xor bh,bh
+  mov ax,1301h
+  int 10h
+  mov si,0
+  ret
