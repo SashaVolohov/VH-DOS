1bd30776f199c6664d1c692b47ec020764440b6c
Larry Holst <45205643+Diicorp95@users.noreply.github.com>
2021-02-14 15:30:59 +0300

    Create UpperCase.asm

---
+++ sources/kernel/UpperCase.asm
@@ -0,0 +1,42 @@
+UpperCase:
+; "Поместить все буквы в верхний регистр"
+; BP - указатель на смещение в памяти, начало строки.
+; В конце строки должен стоять 0x00.
+
+; если ASCII-код символа < 65, это не буква.
+; если ASCII-код символа < 91, это буква в верхнем регистре.
+; если ASCII-код символа < 97, это не буква.
+; если ASCII-код символа < 123, это буква в нижнем регистре.
+	mov si,bp
+	xor bx,bx
+UpperCase@01:
+	cmp bx,0FFFFh
+	jz UpperCase@exit
+	push bp
+	mov bp,bx
+	mov al,[si+bp]
+	pop bp
+	cmp al,0
+	jz UpperCase@exit
+	inc bx
+	cmp al,97
+	jl UpperCase@02 ; doesn't matters
+	cmp al,122
+	jl UpperCase@03 ; our letter - low register
+	jge UpperCase@02 ; doesn't matters
+	jmp UpperCase@01
+UpperCase@02: ; just add
+	push bp
+	mov bp,bx
+	mov [si+bp],al
+	pop bp
+	jmp UpperCase@01
+UpperCase@03: ; process, add
+	sub al,32
+	push bp
+	mov bp,bx
+	mov [si+bp],al
+	pop bp
+	jmp UpperCase@01
+UpperCase@exit:
+	ret
