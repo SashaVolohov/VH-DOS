c13284677e2836dc0ec48776f17f9a86bb70b4bf
Larry Holst <45205643+Diicorp95@users.noreply.github.com>
2021-02-14 16:04:57 +0300

    Not done

--- sources/boot/DOSLDR.asm
+++ sources/boot/DOSLDR.asm
@@ -4,28 +4,24 @@
 ; Данный файл служит для запуска ОС, и загружает основные программы в память.
 
 	org 500h
-
+	
 message:
-	mov ax, 0002h
+	mov ax,2
 	int 10h
         
-	mov dx,0h
 	mov bp,start_dos
-	mov cx,18
-	mov bl,07h					
-	xor bh,bh
-	mov ax,1301h
-	int 10h
-	mov si,0
+	call TxtPrint
 		
 	mov ah,2
 	mov bh,0
 	mov dh,2
 	mov dl,0
 	int 10h
-		
-	mov ax,0000h
-	mov es,ax
+
+	xor ax,ax
+	push word 0
+	pop es
+
 	mov bx,800h
 	mov ch,0
 	mov cl,04h
@@ -33,23 +29,23 @@ message:
 	mov dl,80h
 	mov al,01h
 	mov ah,02h
-	int 13h ; чтение boot файла
-		
-	mov ax,0000h
+	int 13h
+
+	xor ax,ax
 	mov es,ax
-	mov bx,600h
+	mov bx,00600h
 	mov ch,0
-	mov cl,03h
+	mov cl,3
 	mov dh,0
 	mov dl,80h
-	mov al,01h
-	mov ah,02h
+	mov al,1
+	mov ah,2
 	int 13h
 	
-	mov ax, 0002h
+	mov ax,2
 	int 10h
 		
-	mov ah,02h
+	mov ah,2
 	mov bh,0
 	mov dh,0
 	mov dl,0
@@ -58,13 +54,43 @@ message:
 	jmp 0000:0600h	
 	jmp $ ; Возможно, никогда досюда процесс не дойдёт
 
+TxtPrint:
+	mov si,bp
+	xor bx,bx
+TxtPrint@01:
+	cmp bx,0FFFFh
+	jz TxtPrint@02
+	push bx
+	mov bp,bx
+	mov al,[si+bp]
+	pop bx
+	cmp al,0
+	jz TxtPrint@02
+	mov [TxtPrint_Data01],al
+	push ax bx si bp
+	mov ah,0Ah
+	xor bx,bx
+	mov cx,1
+	mov al,[TxtPrint_Data01]
+	int 10h
+	pop bp si bx ax
+	mov ah,03h
+	xor bx,bx
+	int 10h
+	mov ah,02h
+	xor bx,bx
+	inc dl
+	int 10h
+	pop bx
+	inc bx
+	jmp TxtPrint@01
+TxtPrint_Data01 db 0
+TxtPrint@02:
+	xor bx,bx
+	xor bp,bp
+	xor si,si
+	xor ax,ax
+	ret
+
 start_dos db 'Starting VH-DOS...',0
 error_start db 'Cannot find COMMAND.SYS',0
-
-print_mes:
-	mov bl,07h					
-	xor bh,bh
-	mov ax,1301h
-	int 10h
-	mov si,0
-	ret
