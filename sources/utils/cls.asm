47a8539704a4a04549800e90cd8c7a09d78a8b9e
Larry Holst <45205643+Diicorp95@users.noreply.github.com>
2021-02-14 13:41:01 +0300

    Add files via upload

---
+++ sources/utils/cls.asm
@@ -0,0 +1,21 @@
+  org 800h
+
+message:
+  mov ax,2
+  int 10h
+  
+  mov ah,2
+  mov bh,0
+  mov dh,0
+  mov dl,0
+  int 10h
+
+  jmp 0000:0600h
+
+print_mes:
+  mov bl,07h					
+  xor bh,bh
+  mov ax,1301h
+  int 10h
+  mov si,0
+  ret
