f0f51578bec5e46799712deac196a2b589a081c6
Larry Holst <45205643+Diicorp95@users.noreply.github.com>
2021-02-24 21:28:10 +0300

    Update pre_boot.asm

--- sources/setup/pre_boot.asm
+++ sources/setup/pre_boot.asm
@@ -7,9 +7,19 @@
 	org 07C00h
 
 start:
+	mov ah,02h
+	mov al,1
+	xor ch,ch
+	mov cl,2
+	xor dx,dx
+	mov bx,07E00h
+	int 13h
+	mov ax,word ptr [07E00h]
+	cmp ax,OurSignature
 	push word 0 ; Переход на 0000:7E000,
 	push 07E00h ; где и начинается полезный
 	retf        ; загрузчик
 
+file 'oursignature.dat':0,2
 times (512 - 2 - (07C00h - $)) db 0
 db 055h,0AAh
