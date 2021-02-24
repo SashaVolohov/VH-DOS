0d4057a33615c79ba3f28f50a54d71b339fe96ae
Larry Holst <45205643+Diicorp95@users.noreply.github.com>
2021-02-24 17:30:35 +0300

    useless

--- sources/application.asm
+++
@@ -1,9 +0,0 @@
-macro align value { db value - 1 - ($ + value - 1) mod (value) dup 0 }
-HEADS = 1
-SPT = 7
-begin:
-	file "boot\DOSLDR.bin"
-	file "utils\command.bin"
-	file "utils\cls.bin"
-	align 512
-	align HEADS*SPT*512
