6df5ecf8491c45096bcff2f66c62a198739930a6
Larry Holst <45205643+Diicorp95@users.noreply.github.com>
2021-02-24 21:11:48 +0300

    Update macros.asm

--- sources/macros.asm
+++ sources/macros.asm
@@ -1,10 +1,10 @@
 ; include "../macros.asm"
 
 MACRO ClearStack {
-	push qword 0
-	push qword 0
-	pop cs
-	pop ds
+	push qword 0 ; qword - 8 bytes!
+	push qword 0 ; dword - 4 bytes!
+	pop cs ; cs,ds,es,ss - all of them are 4 bytes long!
+	pop ds ; cs+ds+es+ss = 4*4 = 16 bytes
 	pop es
 	pop ss
 }
