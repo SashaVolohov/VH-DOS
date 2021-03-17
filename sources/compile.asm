2c06c37f9ca153c515eeb179ab227c6a9c97a10a
Larry Holst <45205643+Diicorp95@users.noreply.github.com>
2021-03-17 19:06:40 +0300

    Update compile.asm

--- sources/compile.asm
+++ sources/compile.asm
@@ -18,16 +18,30 @@ HEADS = 1
 SPT = 7
 Diskette_Size equ 1474560;bytes
 
-begin:
+begin:	; ДИСКЕТА С УСТАНОВЩИКОМ
+	; СЕКТОР №0
 	file "setup\pre_boot.bin",512 ; Для следующего загрузчика
+
+	; СЕКТОР №1
 	db 0F0h, 0FFh, 0FFh ; 3,5" HD floppy disk
 	zerobytes 512
+	
+	; СЕКТОР №2
 	file "setup\boot.bin",512 ; Первый загрузчик (MBR) установщик > 512 байт
+	
+	; СЕКТОР №3
+	file "setup\boot.bin":512,(512*2)
+	
+	; СЕКТОР №4
 	file "boot\boot.bin",512 ; Загрузчик
+
+	; СЕКТОР №5
 	file "boot\DOSLDR.bin" ; DOSLDR
 	zerobytes 512
+	
+	; СЕКТОР №6
 	file "utils\command.bin" ; COMMAND.SYS
 	zerobytes 512
-	zerobytes HEADS*SPT*512
-
+	
+	zerobytes HEADS*SPT*512 ; idk what is this..
 times (Diskette_Size - ($ - 07C00h)) db 0
