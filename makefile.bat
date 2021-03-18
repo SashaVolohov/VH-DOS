f2b4e24b2fbc385beda5a6dff5d25862c5885d06
Larry Holst <45205643+Diicorp95@users.noreply.github.com>
2021-03-18 22:23:55 +0300

    Add files via upload

--- makefile.bat
+++ makefile.bat
@@ -1,4 +1,5 @@
 @echo off
+title VH-DOS
 set fasm=tools\fasm.exe
 set srcs=sources
 set src_boot=sources\boot
@@ -7,14 +8,18 @@ set src_cmds=sources\utils
 set src_stp=sources\setup
 
 rem Compiling OS
+echo:VH-DOS is compiling...
+echo:VH-DOS build at %date%, %time:~0,8%> build.log
+echo:>> build.log
 for %%a in (%src_boot%\*.asm %src_krn%\*.asm %src_krn%\*.inc %src_cmds%\*.asm %src_stp%\*.asm) do (
-	echo:%%~a
-	%fasm% %%~a
-	echo:
+	echo:...%cd:~-11%\%%~a>> build.log
+	%fasm% %%~a >> build.log
+	echo:>> build.log
 	if "%errorlevel%" gtr "0" (goto endofcompl)
 )
 
 rem Making floppy disk image
+echo:Building floppy disk image...
 %fasm% %srcs%\compile.asm
 copy %srcs%\compile.bin %cd%\setup.img> nul
 del %srcs%\*.bin> nul
